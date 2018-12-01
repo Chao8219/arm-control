6f7c942b2cad137d8ee57c9fa9acb34e28543abd
Zichao Hou <zichao@gwu.edu>
2018-12-01 16:04:06 -0500

    tcpsocket client end added

---
+++ tcpsocket/TCP_Socket_Client_Example.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = '161.253.72.254'  # The server's hostname or IP address
+PORT = 9090        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
